a06ac6dcb2da27fe28962d069c6a571c559e0cdf
tbsdvl <tristonburns97@gmail.com>
2023-04-25 02:17:11 +0000

    Add a while loop to add projects

--- src/index.js
+++ src/index.js
@@ -15,15 +15,27 @@ const app = async () => {
     branch: taskPromptAnswers.branch,
   });
 
-  const projectPromptAnswers = await designer.getProjectPromptAnswers();
-  if (newTask.projects[projectPromptAnswers.project]) {
-    // log an error message
-    console.log("Project already exists in the task.");
-    // add handling to allow users to overwrite the project in the task
-    // or continue to add a new task
-  } else {
-    newTask.addProject(projectPromptAnswers.project);
+  // create a while condition that checks to whether or not
+  // to exit the loop
+  while (true) {
+    const projectPromptAnswers = await designer.getProjectPromptAnswers();
+    if (newTask.projects[projectPromptAnswers.project]) {
+      // log an error message
+      console.log("Project already exists in the task.");
+      // add handling to allow users to overwrite the project in the task
+      // or continue to add a new task
+    } else {
+      newTask.addProject(projectPromptAnswers.project);
+    }
+
+    if (projectPromptAnswers.addProject) {
+      continue;
+    } else {
+      break;
+    }
   }
+
+  console.log('Here');
 };
 
 app();
