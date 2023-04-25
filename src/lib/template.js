e7212cbd88a1fe3f0d151197a7c9d7f7e9b05759
tbsdvl <tristonburns97@gmail.com>
2023-04-25 05:17:42 +0000

    Update documentation

--- src/lib/template.js
+++ src/lib/template.js
@@ -4,6 +4,7 @@ module.exports = {
   /**
    * Creates a task template
    * @param {Task} task The task.
+   * @returns {string} The task template string.
    */
   createTaskTemplate: (task) => {
     const template = 
